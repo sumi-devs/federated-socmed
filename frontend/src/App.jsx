eba51988974a87c7cc7c9696f4d79ca3d3787997
sumi-devs <sumithra13022004@gmail.com>
2026-01-30 23:34:12 +0530

    App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import './styles/app.css';
+import Profile from './pages/Profile';
+import Channels from './pages/Channels';
+import Settings from './pages/Settings';
+import ServerDetails from './pages/ServerDetails';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/channels" element={<Channels />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/server-details" element={<ServerDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
